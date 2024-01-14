36c7124b3dfd1e5f7d2ac1f55ef5465ed63cfd74
MarcinCichy <marcin-cichy@wp.pl>
2024-01-14 02:20:32 +0100

    Added highest priority element queue.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-basic_example_queue = ['element1', 'element2', 'element3', 'element4', 'element5']
-tup_example_queue = [(1, 'element1'), (2, 'element2'), (3, 'element3'), (5, 'element4'), (5, 'element5')]
-dic_example_queue = [{'element1': 1}, {'element2': 2}, {'element3': 3}, {'element4': 5}, {'element5': 5}]
+tup_example_queue = [('element1', 1), ('element2', 2), ('element3', 3), ('element4', 5), ('element5', 5)]
 
 
 class Queue:
@@ -11,55 +9,52 @@ class Queue:
         self.queue.append(new_element)
 
     def take_and_remove_first_element(self):
-        element = self.queue.pop(0)
-        return element
+        self.queue.pop(0)
 
     def take_and_show_first_element(self):
-        element_to_show = self.queue[0]
-        return element_to_show
+        return self.queue[0]
 
     def is_queue_empty(self):
-        len_of_queue = self.show_length_of_queue()
-        if len_of_queue == 0:
-            print(f'Queue is empty')
-        else:
-            print(f'Queue is not empty')
+        if len(self.queue) == 0:
+            return print(f"Queue is empty")
 
     def show_length_of_queue(self):
-        len_of_queue = len(self.queue)
-        return len_of_queue
+        return len(self.queue)
 
 
 class PriorityQueue(Queue):
-    def __init__(self):
+    def __init__(self, queue):
         super().__init__(queue)
 
     def take_and_remove_high_priority_element(self):
-        pass
+        highest_priority_element = max(self.queue, key=lambda x: x[1])
+        self.queue.remove(highest_priority_element)
 
 
-# dic_example_queue
-# tup_example_queue
-print(f'Show org queue: {dic_example_queue}')
-queue = Queue(dic_example_queue)
+print(f'Show org queue: {tup_example_queue}')
+queue = Queue(tup_example_queue)
+print(f"Show length of org queue: {queue.show_length_of_queue()}")
 
-print(f"Show length of queue: {queue.show_length_of_queue()}")
-queue.is_queue_empty()
+print(f"Add element to queue")
+# queue.add_element({'element6': 3})
+queue.add_element(('element6', 3))
+print(f'Show queue after add element: {queue.queue}')
+print("="*63)
+#
+# for element in list(queue.queue):
+#     print(f"Queue = {queue.queue}")
+#     print(f"Element = {element}")
+#     print(f"Show length of queue: {queue.show_length_of_queue()}")
+#     print(f"Show queue's first element: {queue.take_and_show_first_element()}")
+#     queue.take_and_remove_first_element()
+#     print(f'Show queue after remove first element: {queue.queue}')
+#     print("-"*63)
+#
+#     if queue.is_queue_empty():
+#         break
 
-queue.add_element({'element6': 3})
-# queue.add_element((5, 'element6'))
-print(f'Show queue after add element: {dic_example_queue}')
-print(f"Show queue's first element: {queue.take_and_show_first_element()}")
-
-
-print(f"Show length of queue: {queue.show_length_of_queue()}")
-
-queue.take_and_remove_first_element()
-print(f'Show queue after remove first element: {dic_example_queue}')
-
-
-print(f"Show queue's first element: {queue.take_and_show_first_element()}")
-
-dic_example_queue.clear()
-# tup_example_queue.clear()
-queue.is_queue_empty()
+highpr_queue = PriorityQueue(tup_example_queue)
+highpr_queue.take_and_remove_high_priority_element()
+highpr_queue.show_length_of_queue()
+highpr_queue.take_and_show_first_element()
+print(f"Queue after remove highpriority element = {highpr_queue.queue}")
